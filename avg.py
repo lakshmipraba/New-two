b0bc888e7bc063a67a1de1c843acd3e4c1c2e153
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-03 11:35:36 +0530

    Create avg.py

---
+++ avg.py
@@ -0,0 +1,7 @@
+a=raw_input("number a:")
+b=raw_input("number b:")
+c=raw_input("number c:")
+print "The numbers are:"
+print a,b,c
+print "Average of the numbers is:"
+print float(int(a)+int(b)+int(c))/3
